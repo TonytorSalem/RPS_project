33004eeb9bfaddd5e2f67f40508904a5f41960e2
Tonytor <teme4842@gmail.com>
2023-05-17 09:09:51 +0300

    creating a getcomputerChoice function that randomly return either rock paper scissor

---
+++ index.js
@@ -0,0 +1,15 @@
+const arr = ['Rock','Paper','Scissor'];
+
+function getComputerChoise(){
+  console.log(arr[(Math.floor(Math.random()*arr.length))])
+}
+
+/*
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));*/
